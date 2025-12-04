698d5ab400186ad5609b8947e61aedbb4f486e21
Jakub Pogran <jakub.pogran@cookielab.io>
2025-12-04 09:40:11 +0100

    day two part two

--- day2.ts
+++ day2.ts
@@ -1,13 +1,13 @@
 import { loadInputs } from "./load-inputs";
 
-const input = await loadInputs("day2");
+const input = await loadInputs("day2", true);
 let total = 0;
 
 for (const range of input.split(",")) {
   const [start, end] = range.split("-").map((num) => parseInt(num, 10));
 
   for (let i = start; i <= end; i++) {
-    if (/^(.+)\1$/.test(i.toString())) total += i;
+    if (/^(.+)\1+$/.test(i.toString())) total += i;
   }
 }
 
