6f3249635780fcbf3d38dbaca9b9cc5759b82bdd
Jakub Pogran <jakub.pogran@cookielab.io>
2025-12-04 10:28:44 +0100

    day three part two

--- day3.ts
+++ day3.ts
@@ -3,21 +3,27 @@ import { loadInputs } from "./load-inputs";
 const input = await loadInputs("day3");
 let total = 0;
 
-for (const bank of input.split("\n")) {
-  let first = 0;
-  let second = 0;
+function zeros(num: number): number[] {
+  return Array.from({ length: num }, () => 0);
+}
 
+for (const bank of input.split("\n")) {
+  let batteries = zeros(12);
   for (let i = 0; i < bank.length; i++) {
     const digit = parseInt(bank[i], 10);
-    if (digit > first && i < bank.length - 1) {
-      first = digit;
-      second = 0;
-    } else if (digit > second) {
-      second = digit;
+
+    for (let j = 0; j < batteries.length; j++) {
+      const rest = batteries.length - j - 1;
+      if (digit > batteries[j] && bank.length - 1 - i >= rest) {
+        batteries[j] = digit;
+        batteries = [...batteries.slice(0, j + 1), ...zeros(rest)];
+        break;
+      }
     }
   }
-  console.log([bank, first, second]);
-  total += 10 * first + second;
+  total += batteries
+    .reverse()
+    .reduce((acc, val, idx) => acc + val * 10 ** idx, 0);
 }
 
 console.log(total);
